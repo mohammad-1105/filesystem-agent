b7574237bc8d352b5d71d0f057fea00c33a4e313
Mohammand Aman <mohammadamaan1105@gmail.com>
2026-02-27 00:18:43 +0545

    feat(agent): init sandbox and connect bash tool

--- lib/agent.ts
+++ lib/agent.ts
@@ -1,8 +1,13 @@
 import { openai } from "@ai-sdk/openai";
+import { Sandbox } from "@vercel/sandbox";
 import { ToolLoopAgent } from "ai";
+import { createBashTool } from "./tools";
 
+const sandbox = await Sandbox.create();
 export const agent = new ToolLoopAgent({
   model: openai("gpt-5.2"),
   instructions: "",
-  tools: {},
+  tools: {
+    bashTool: createBashTool(sandbox),
+  },
 });
